30fec696b57e90f25c723762d240504064ff34b4
yoga-bayu <yogabayuaditya20@gmail.com>
2026-01-03 10:49:31 +0700

    new

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,7 +85,7 @@ module.exports = {
         background_color: `#ebebfa`,
         theme_color: `#ebebfa`,
         display: `standalone`,
-        icon: `src/images/3D-liquid-abstract-5.webp`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
