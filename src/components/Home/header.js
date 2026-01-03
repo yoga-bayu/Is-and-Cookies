769e7dfef33a142cee601cefd2942bb7f4ca74c8
yoga-bayu <yogabayuaditya20@gmail.com>
2026-01-03 10:09:55 +0700

    new

--- src/components/Home/header.js
+++ src/components/Home/header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Fade from "react-reveal/Fade"
 import "../../styles/global.css"
-import hero from "../../../../holo-theme/static/img/logo.png"
+import hero from "../../../static/img/Github-Logo.png"
 
 const Header = () => {
   return (
